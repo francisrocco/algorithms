0adc64ece27fd187f3dee2e6a6fc0e81e994885b
App Academy Student <appacademy@Apps-Mac-mini-24.local>
2016-06-17 16:33:07 -0400

    begin dfs method

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -11,30 +11,19 @@ class PolyTreeNode
     @children = []
   end
 
-  # def parent=2(new_parent)
-  #   if @parent != nil
-  #     @parent.children.delete(self)
-  #   end
-  #     @parent = new_parent
-  #     @parent.children << self unless @parent.children.include?(self)
-  #
-  # end
-
   def parent=(new_parent)
-    # if @parent != nil
-    #   @parent.children.delete(self)
-    #   @parent = new_parent
-    #   @parent.children << self unless @parent.children.include?(self)
-    # end
-    #   @parent = new_parent
-    unless @parent.nil?
-     @parent.children.delete(self)
-    end
+    @parent.children.delete(self) unless @parent.nil?
     @parent = new_parent
-    unless @parent.nil?
-     @parent.children << self
-    end
-    
+    @parent.children << self unless @parent.nil?
+  end
+
+  def add_child(child_node)
+    child_node.parent=(self)
+  end
+
+  def remove_child(child_node)
+    raise "Incorrect" unless self.children.include?(child_node)
+    child_node.parent=(nil)
   end
 
   def to_s
@@ -43,6 +32,16 @@ class PolyTreeNode
 
   def inspect
     to_s
+  end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+    return nil if self.children.empty?
+    children.each do |child|
+      dfs(child)
+      return child.value if child.value == target_value
+    end
+    return nil
   end
 
 end
