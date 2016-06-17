c747dee9cd911d07f002f1e871a8eea92b2477f7
App Academy Student <appacademy@Apps-Mac-mini-24.local>
2016-06-17 15:28:04 -0400

    define parent= method

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,6 +1,9 @@
+require "byebug"
+
 class PolyTreeNode
 
   attr_accessor :value, :children
+  attr_reader :parent
 
   def initialize(value)
     @value = value
@@ -8,14 +11,38 @@ class PolyTreeNode
     @children = []
   end
 
-  def parent
-    @parent
+  # def parent=2(new_parent)
+  #   if @parent != nil
+  #     @parent.children.delete(self)
+  #   end
+  #     @parent = new_parent
+  #     @parent.children << self unless @parent.children.include?(self)
+  #
+  # end
+
+  def parent=(new_parent)
+    # if @parent != nil
+    #   @parent.children.delete(self)
+    #   @parent = new_parent
+    #   @parent.children << self unless @parent.children.include?(self)
+    # end
+    #   @parent = new_parent
+    unless @parent.nil?
+     @parent.children.delete(self)
+    end
+    @parent = new_parent
+    unless @parent.nil?
+     @parent.children << self
+    end
+    
   end
 
-  def parent=(parent_prop)
-    return nil if parent_prop.nil?
-    @parent = parent_prop
-    parent_prop.children << self
+  def to_s
+    value
+  end
+
+  def inspect
+    to_s
   end
 
 end
